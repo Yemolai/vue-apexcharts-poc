0f4a648577969627e95ad83dcc1144e9eb5172fc
Romulo Gabriel Rodrigues <yemolai@gmail.com>
2019-08-26 09:34:39 -0300

    creating bar chart component

---
+++ src/components/BasicBarChart.vue
@@ -0,0 +1,43 @@
+<template>
+  <apex-chart :width="width" type="bar" :options="chartOptions" :series="chartSeries"/>
+</template>
+
+<script>
+import ApexChart from 'vue-apexcharts'
+export default {
+  name: 'BasicBarChart',
+  components: { ApexChart },
+  props: {
+    width: {
+      type: Number,
+      default: 500
+    },
+    options: {
+      type: Object,
+      default: () => ({})
+    },
+    xAxis: {
+      type: Object,
+      default: () => ({})
+    },
+    data: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  computed: {
+    chartOptions () {
+      return {
+        xaxis: this.xAxis
+      }
+    },
+    chartSeries () {
+      return Object.keys(this.data).map(k => ({ name: k, data: this.data[k] }))
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
