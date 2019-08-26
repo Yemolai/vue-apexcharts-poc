d0d9925d87ae613a68196830ecb54a3c8858c314
Romulo Gabriel Rodrigues <yemolai@gmail.com>
2019-08-26 09:34:47 -0300

    implementing bar chart component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,36 +6,41 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha" target="_blank" rel="noopener">unit-mocha</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h3>ApexCharts</h3>
+    <basic-bar-chart :x-axis="{ years }" :data="chartData" />
   </div>
 </template>
 
 <script>
+import BasicBarChart from './BasicBarChart'
+
+const years = new Array(12).fill(0).map((_, idx, a) => Number(new Date().getFullYear()) - a.length + idx)
+
 export default {
   name: 'HelloWorld',
+  components: { BasicBarChart },
   props: {
     msg: String
+  },
+  data: () => ({
+    years,
+    series: 2,
+    length: 12
+  }),
+  computed: {
+    xData () {
+      const y = new Date().getFullYear()
+      return new Array(this.length).fill(0).map((_, idx, a) => Number(y) - a.length + idx)
+    },
+    chartData () {
+      const len = this.years.length
+      const data = new Array(this.series).fill(0).reduce((a, c) => {
+        const newData = new Array(len).fill(0)
+          .map((_, idx) => Number(((Math.random() * 2) + idx).toFixed(2)))
+        return { ...a, [`DataSet ${c + 1}`]: newData }
+      })
+      return data
+    }
   }
 }
 </script>
